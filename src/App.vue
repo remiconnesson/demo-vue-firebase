83941f8847050b1ba9f162599326330193f3907a
Rémi Connesson <remiconnesson@gmail.com>
2022-10-21 12:14:41 +0200

    Factorize out auth and app logic

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,5 @@
 <script setup>
-import { ref } from "vue"
-import { initializeApp } from "firebase/app";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-
-const provider = new GoogleAuthProvider();
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBlg_1iHV3llX_Ky2qAPYZYVcCuxC9sOD4",
-  authDomain: "web-2022-c850d.firebaseapp.com",
-  projectId: "web-2022-c850d",
-  storageBucket: "web-2022-c850d.appspot.com",
-  messagingSenderId: "505208230679",
-  appId: "1:505208230679:web:6894ed3628b6c3b28c9d52"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
-
-function loginPopup() {
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      console.log('Log in')
-    }).catch((error) => {
-      console.log('Error')
-    });
-}
-
-const uid = ref(null) 
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    uid.value = user.uid;
-    console.log(user)
-  } else {
-    uid.value = null;
-  }
-});
-
+import { auth, uid, loginPopup } from "@/auth"
 </script>
 
 <template>
---
+++ src/initFirebaseApp.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBlg_1iHV3llX_Ky2qAPYZYVcCuxC9sOD4",
+  authDomain: "web-2022-c850d.firebaseapp.com",
+  projectId: "web-2022-c850d",
+  storageBucket: "web-2022-c850d.appspot.com",
+  messagingSenderId: "505208230679",
+  appId: "1:505208230679:web:6894ed3628b6c3b28c9d52"
+};
+const app = initializeApp(firebaseConfig);
+
+export { app }
